688eb5940ebd9b2e77748176eb8c93d04e320a39
unknown <flucasjs@gmail.com>
2024-02-25 19:45:43 -0800

    Add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+import { Fonts } from "../interfaces";
+
+export const REGISTERED_FONTS: Fonts = {
+  'font-sans': 'Sans Serif',
+  'font-serif': 'Serif',
+  'font-mono': 'Mono',
+};
+
+export const DEFAULT_FONT = 'font-serif';
